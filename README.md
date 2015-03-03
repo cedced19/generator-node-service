0ebaf4360c444ec8bfd92bb551c5007428fcacbc
Cédric <cedced19@gmail.com>
2015-03-03 20:47:32 +0100

    Adding new file, do a beautiful code adding Travis bage

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Node Service  Generator
+[![Build Status](https://travis-ci.org/cedced19/generator-node-service.svg)](https://travis-ci.org/cedced19/generator-node-service)
 __Work in progress__
--- app/index.js
+++ app/index.js
@@ -4,83 +4,79 @@ var path = require('path');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
 var chalk = require('chalk');
-
-
+var slug = require('slug');
 var NodeServiceGenerator = yeoman.generators.Base.extend({
   init: function () {
     this.pkg = require('../package.json');
-
     this.on('end', function () {
       if (!this.options['skip-install']) {
         this.installDependencies();
       }
     });
   },
-
   askFor: function () {
     var done = this.async();
-
     // Have Yeoman greet the user.
     this.log(yosay('Welcome to the marvelous static angular generator!'));
-
-    var prompts = [{
-      name: 'title',
-      message: 'What is the title of your application?',
-      default: 'Hello World'
-    },{
-      name: 'description',
-      message: 'Please describe your app:'
-    },{
-      name: 'name',
-      message: 'What is your name?'
-    },{
-      name: 'github',
-      message: 'What is your github?'
-    },{
-      name: 'email',
-      message: 'What is your email?'
-    }];
-
+    var prompts = [
+      {
+        name: 'title',
+        message: 'What is the title of your application?',
+        default: 'Hello World'
+      },
+      {
+        name: 'description',
+        message: 'Please describe your app:',
+        default: 'A app.'
+      },
+      {
+        name: 'name',
+        message: 'What is your name?',
+        default: 'C\xE9dric'
+      },
+      {
+        name: 'github',
+        message: 'What is your github?',
+        default: 'cedced19'
+      },
+      {
+        name: 'email',
+        message: 'What is your email?',
+        default: 'user@domain.com'
+      },
+      {
+        type: 'confirm',
+        name: 'socket',
+        message: 'Would you like socket.io?',
+        default: false
+      }
+    ];
     this.prompt(prompts, function (props) {
       this.title = props.title;
       this.description = props.description;
       this.name = props.name;
       this.github = props.github;
       this.email = props.email;
-
+      this.socket = props.socket;
       done();
-
-      var extractGeneratorName = function (_, appname) {
-        var slugged = _.slugify(title);
-        var match = slugged.match(/^$/);
-
-        if (match && match.length === 2) {
-          return match[1].toLowerCase();
-      }
-
-      return slugged;
-      };
-        }.bind(this));
-      },
+    }.bind(this));
+  },
     
-
-    app: function () {
-      this.mkdir('lib');
-      this.mkdir('vendor/js');
-      this.mkdir('vendor/css');
-      this.mkdir('vendor/fonts');
-      this.mkdir('dist')
-
-      this.template('index.html', 'index.html');
-      this.template('vendor/js/app.js', 'vendor/js/app.js');
-      this.template('vendor/css/main.css', 'vendor/css/main.css');
-      this.template('_package.json', 'package.json');
-      this.copy('gitignore', '.gitignore');
-      this.copy('gitignore', '.gitignore');
-      this.template('Gruntfile.js', 'Gruntfile.js');
-      this.template('README.md', 'README.md');
-    }
-  });
-
-
+  app: function () {
+    this.mkdir('lib');
+    this.mkdir('vendor/js');
+    this.mkdir('vendor/css');
+    this.mkdir('vendor/fonts');
+    this.mkdir('dist');
+    this.template('index.html', 'index.html');
+    this.template('vendor/js/app.js', 'vendor/js/app.js');
+    this.template('vendor/css/main.css', 'vendor/css/main.css');
+    this.template('_package.json', 'package.json');
+    this.template('app.js', slug(this.title).toLowerCase() + '.js');
+    this.copy('gitignore', '.gitignore');
+    this.copy('gitignore', '.gitignore');
+    this.template('Gruntfile.js', 'Gruntfile.js');
+    this.template('README.md', 'README.md');
+  }
+});
 module.exports = NodeServiceGenerator;
